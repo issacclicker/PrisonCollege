798abdd428c15feddc6b046e418f33403c16d993
kingdoza <kdowon2k98@naver.com>
2026-01-07 21:56:53 +0900

    다각도 돌진

--- Assets/Scripts/BehaveSpots/ExitSpot.cs
+++ Assets/Scripts/BehaveSpots/ExitSpot.cs
@@ -8,6 +8,7 @@ public class ExitSpot : SingleStudentSpot
 
     public bool CanExit => !_exitGate.IsBarricadePlaced;
     public ExitGateType GateType => _exitGate.GateType;
+    public Vector3 GatePosition => _exitGate.transform.position;
 
 
 
