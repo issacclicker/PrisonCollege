b0798149c34dd5512cad66dc6b8ccb644dad9622
kingdoza <kdowon2k98@naver.com>
2026-02-02 13:18:03 +0900

    해킹 라이트 onoff

---
+++ Assets/Scripts/FuseBox.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FuseBox : MonoBehaviour
+{
+    private ClickAndWait _interaction;
+
+
+
+    private void Awake()
+    {
+        _interaction = GetComponent<ClickAndWait>();
+        _interaction.ProgressCompleteEvent.AddListener(TurnOnLights);
+        LabLightSystem.Instance.LightsOffEvent.AddListener(() => _interaction.SetInteractable(true));
+        LabLightSystem.Instance.LightsOnEvent.AddListener(() => _interaction.SetInteractable(false));
+    }
+
+
+    private void TurnOnLights()
+    {
+        LabLightSystem.Instance.TurnOn();
+    }
+}
--- Assets/Scripts/Systems/LabLightSystem.cs
+++ Assets/Scripts/Systems/LabLightSystem.cs
@@ -1,16 +1,45 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class LabLightSystem : SceneSingleton<LabLightSystem>
 {
+    [SerializeField] private GameObject _toggleableLightGroup;
+    public bool IsLightsOn => _toggleableLightGroup.activeSelf;
+
+    [HideInInspector] public UnityEvent LightsOffEvent = new();
+    [HideInInspector] public UnityEvent LightsOnEvent = new();
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+
+
+
+    private void Start()
+    {
+        TurnOn();
+    }
+
+
+
     public void TurnOff()
     {
+        if (!IsLightsOn) return;
         Debug.Log("LightOff");
+        _toggleableLightGroup.SetActive(false);
+        LightsOffEvent?.Invoke();
     }
 
 
 
     public void TurnOn()
     {
+        if (IsLightsOn) return;
         Debug.Log("LightOn");
+        _toggleableLightGroup.SetActive(true);
+        LightsOnEvent?.Invoke();
     }
 }
