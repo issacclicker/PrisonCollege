9f4daa04439f01b1ee1070ffcd25b0887174dd08
kingdoza <kdowon2k98@naver.com>
2026-02-10 11:46:44 +0900

    ThrowWeapon2

---
+++ Assets/Scripts/Weapons/ThrowWeapon2.cs
@@ -0,0 +1,86 @@
+using DigitalRuby.RainMaker;
+using UnityEngine;
+
+public class ThrowWeapon2 : RangedWeapon
+{
+    [SerializeField] private float _throwVelocity;
+    [SerializeField] private float _flipVelocity;
+    [SerializeField] private float _torqueRandomness;
+    private ThrowAnimator _throwAnimator;
+
+
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _throwAnimator = GetComponent<ThrowAnimator>();
+    }
+
+
+    protected override void Shot(Vector3 viewportPoint)
+    {
+        Vector3 shotDestination = GetShotDestination(viewportPoint);
+        //Debug.DrawRay(shotDestination, Vector3.up * 0.5f, Color.green, 1.0f);
+        //Debug.DrawRay(shotDestination, Vector3.right * 0.5f, Color.green, 1.0f);
+        Vector3 shotDirection = (shotDestination - _spawnPoint.position).normalized;
+        Quaternion projectileRot = Camera.main.transform.rotation * _spawnPoint.localRotation;
+        GameObject projectileSpawned = Instantiate(_projectilePrefab, _spawnPoint.position, projectileRot);
+        projectileSpawned.transform.localScale = _spawnPoint.localScale;
+        Projectile projectile = projectileSpawned.GetComponent<Projectile>();
+        projectile.WeaponData = _weaponData;
+        projectile.Owner = _owner;
+
+        projectile.ResetForce();
+        projectile.AddVelocityForce(shotDirection, _throwVelocity);
+
+        Vector3 randomTorque = new Vector3(
+                Random.Range(-_torqueRandomness, _torqueRandomness),
+                Random.Range(-_torqueRandomness, _torqueRandomness),
+                Random.Range(-_torqueRandomness, _torqueRandomness)
+            );
+
+        projectile.AddTorqueForce(Camera.main.transform.right + randomTorque, _flipVelocity);
+    }
+
+
+
+    private Vector3 GetShotDestination(Vector3 viewportPoint)
+    {
+        Ray ray = Camera.main.ViewportPointToRay(viewportPoint);
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out RaycastHit hit, _maxDistance))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            // 허공을 쐈을 때
+            targetPoint = ray.GetPoint(_maxDistance);
+        }
+        return targetPoint;
+    }
+
+
+
+    protected override bool Acquire(int count)
+    {
+        if (base.Acquire(count) == false) return false;
+        _throwAnimator.PlayRefillAnimation();
+        return true;
+    }
+
+
+    protected override void CheckBullet()
+    {
+        base.CheckBullet();
+        if (!_magazine.IsDepleted)
+        {
+            _throwAnimator.PlayRefillAnimation();
+        }
+        else
+        {
+            _spawnPoint.gameObject.SetActive(false);
+        }
+    }
+}
