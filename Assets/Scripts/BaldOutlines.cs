c0fa944535f1ef3597ca4105e239d9ee0530d5b7
kingdoza <kdowon2k98@naver.com>
2026-02-12 20:05:12 +0900

    빛별로 탈모빔 적용

--- Assets/Scripts/BaldOutlines.cs
+++ Assets/Scripts/BaldOutlines.cs
@@ -1,14 +1,41 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BaldOutlines : MonoBehaviour
 {
+    private Outline[] outlines;
+    private bool _activationState;
+
+
+    private void Awake()
+    {
+        outlines = GetComponentsInChildren<Outline>();
+        LabLightSystem.Instance.LightsOffEvent.AddListener(() => OnLightChanged(false));
+        LabLightSystem.Instance.LightsOnEvent.AddListener(() => OnLightChanged(true));
+    }
+
+
+
+
     private void Start()
     {
-        bool hasToActivate = AttributeSystem.Instance.IsStudOutline;
-        Outline[] outlines = GetComponentsInChildren<Outline>();
+        _activationState = AttributeSystem.Instance.IsStudOutline;
+        SetOutlines(_activationState);
+    }
+
+
+    private void SetOutlines(bool hasToEnable)
+    {
         foreach (Outline outline in outlines)
         {
-            outline.enabled = hasToActivate;
+            outline.enabled = hasToEnable;
         }
+    }
+
+
+    private void OnLightChanged(bool isLightOn)
+    {
+        if (!_activationState) return;
+        SetOutlines(isLightOn);
     }
 }
