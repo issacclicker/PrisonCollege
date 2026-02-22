41d91d5abea1a90ba3aac154370adc89daca3014
kingdoza <kdowon2k98@naver.com>
2026-02-22 16:57:45 +0900

    메인브금추가&카메라셰이크 끊김 해결

--- Assets/Scripts/CameraShaker.cs
+++ Assets/Scripts/CameraShaker.cs
@@ -19,44 +19,52 @@ public class CameraShaker : SceneSingleton<CameraShaker>
     public void DoDamagedShake(float amount)
     {
         float shakeStrength = amount * _damageShakePosAmount;
-        _mainCamera.transform.DOComplete();
+        _mainCamera.transform.parent.DOComplete();
         _weaponCamera.transform.DOComplete();
-        _mainCamera.transform.DOShakePosition(0.25f, shakeStrength, 25, 90);
-        _weaponCamera.transform.DOShakePosition(0.25f, shakeStrength * _weaponCameraShakeAmount, 25, 90);
+        //_mainCamera.transform.parent.DOKill(true);
+        //_weaponCamera.transform.DOKill(true);
+        _mainCamera.transform.parent.DOShakePosition(0.25f, shakeStrength, 25, 90).SetRelative(true).SetUpdate(UpdateType.Late);
+        _weaponCamera.transform.DOShakePosition(0.25f, shakeStrength * _weaponCameraShakeAmount, 25, 90).SetRelative(true).SetUpdate(UpdateType.Late);
     }
 
 
 
     public void DoRecoilShake(float amount)
     {
-        _mainCamera.transform.DOComplete();
+        _mainCamera.transform.parent.DOComplete();
         _weaponCamera.transform.DOComplete();
-        _mainCamera.transform.DOShakePosition(0.1f, amount * _recoilShakePosAmount, 80, 90).SetRelative(true);
-        _mainCamera.transform.DOShakeRotation(0.1f, amount * _recoilShakeRotAmount, 80, 90);
-        _weaponCamera.transform.DOShakePosition(0.1f, amount * _recoilShakePosAmount * 0.05f, 80, 90).SetRelative(true);
-        _weaponCamera.transform.DOShakeRotation(0.1f, amount * _recoilShakeRotAmount * 0.05f, 80, 90);
+        //_mainCamera.transform.parent.DOKill(true);
+        //_weaponCamera.transform.DOKill(true);
+        _mainCamera.transform.parent.DOShakePosition(0.1f, amount * _recoilShakePosAmount, 80, 90).SetRelative(true).SetRelative(true).SetUpdate(UpdateType.Late);
+        _mainCamera.transform.parent.DOShakeRotation(0.1f, amount * _recoilShakeRotAmount, 80, 90).SetRelative(true).SetUpdate(UpdateType.Late);
+        _weaponCamera.transform.DOShakePosition(0.1f, amount * _recoilShakePosAmount * 0.05f, 80, 90).SetRelative(true).SetRelative(true).SetUpdate(UpdateType.Late);
+        _weaponCamera.transform.DOShakeRotation(0.1f, amount * _recoilShakeRotAmount * 0.05f, 80, 90).SetRelative(true).SetUpdate(UpdateType.Late);
     }
 
 
     public void DoExplosionShake(float amount)
     {
-        _mainCamera.transform.DOComplete();
+        _mainCamera.transform.parent.DOComplete();
         _weaponCamera.transform.DOComplete();
-        _mainCamera.transform.DOShakePosition(0.3f, amount * _explosionShakePosAmount, 50, 90).SetRelative(true);
-        _mainCamera.transform.DOShakeRotation(0.3f, amount * _explosionShakeRotAmount, 50, 90);
-        _weaponCamera.transform.DOShakePosition(0.3f, amount * _explosionShakePosAmount * _weaponCameraShakeAmount, 40, 90).SetRelative(true);
-        _weaponCamera.transform.DOShakeRotation(0.3f, amount * _explosionShakeRotAmount * _weaponCameraShakeAmount, 40, 90);
+        //_mainCamera.transform.parent.DOKill(true);
+        //_weaponCamera.transform.DOKill(true);
+        _mainCamera.transform.parent.DOShakePosition(0.3f, amount * _explosionShakePosAmount, 50, 90).SetRelative(true).SetRelative(true).SetUpdate(UpdateType.Late);
+        _mainCamera.transform.parent.DOShakeRotation(0.3f, amount * _explosionShakeRotAmount, 50, 90).SetRelative(true).SetUpdate(UpdateType.Late);
+        _weaponCamera.transform.DOShakePosition(0.3f, amount * _explosionShakePosAmount * _weaponCameraShakeAmount, 40, 90).SetRelative(true).SetRelative(true).SetUpdate(UpdateType.Late);
+        _weaponCamera.transform.DOShakeRotation(0.3f, amount * _explosionShakeRotAmount * _weaponCameraShakeAmount, 40, 90).SetRelative(true).SetUpdate(UpdateType.Late);
     }
 
 
 
     public void DoMeleeShake(float amount)
     {
-        _mainCamera.transform.DOComplete();
+        _mainCamera.transform.parent.DOComplete();
         _weaponCamera.transform.DOComplete();
+        //_mainCamera.transform.parent.DOKill(true);
+        //_weaponCamera.transform.DOKill(true);
         //_mainCamera.transform.DOShakePosition(0.1f, amount * _meleeShakePosAmount, 80, 90).SetRelative(true);
-        _mainCamera.transform.DOShakeRotation(0.15f, amount * _meleeShakeRotAmount, 80, 90);
+        _mainCamera.transform.parent.DOShakeRotation(0.15f, amount * _meleeShakeRotAmount, 80, 90).SetRelative(true).SetUpdate(UpdateType.Late);
         //_weaponCamera.transform.DOShakePosition(0.1f, amount * _meleeShakePosAmount * _weaponCameraShakeAmount, 60, 90).SetRelative(true);
-        _weaponCamera.transform.DOShakeRotation(0.15f, amount * _meleeShakeRotAmount * _weaponCameraShakeAmount, 60, 90);
+        _weaponCamera.transform.DOShakeRotation(0.15f, amount * _meleeShakeRotAmount * _weaponCameraShakeAmount, 60, 90).SetRelative(true).SetUpdate(UpdateType.Late);
     }
 }
